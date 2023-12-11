f335906bfe748ba698e219260a87eac79850f3d9
Naveed Ali <naveed@digimarkdevelopers.com>
2023-12-12 01:38:23 +0500

    12 hours email restriction, and consultant name

--- controllers/PatientController.js
+++ controllers/PatientController.js
@@ -1,23 +1,30 @@
-const PatientRepo = require('../repo/PatientRepo');
-const { badRequest, successResponse, errorResponse } = require('../config/responceHandler');
-const cloudinary = require('cloudinary').v2;
-const nodemailer = require('nodemailer')
+const PatientRepo = require("../repo/PatientRepo");
+const {
+    badRequest,
+    successResponse,
+    errorResponse,
+} = require("../config/responceHandler");
+const cloudinary = require("cloudinary").v2;
+const nodemailer = require("nodemailer");
+const patientModel = require("../models/PatientModel");
+const userModel = require("../models/UserModel");
+const httpStatus = require("http-status");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.SECRET_KEY,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.SECRET_KEY,
 });
 /**
  * @returns {Array}
  */
 const getAllPatients = async (req, res, next) => {
-  try {
-    const patients = await PatientRepo.getAllPatients();
-    successResponse(res, 'Patients retrieved successfully.', patients, 200);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const patients = await PatientRepo.getAllPatients();
+        successResponse(res, "Patients retrieved successfully.", patients, 200);
+    } catch (error) {
+        next(error);
+    }
 };
 
 /**
@@ -25,40 +32,59 @@ const getAllPatients = async (req, res, next) => {
  * @returns {PatientModel}
  */
 const createPatient = async (req, res, next) => {
-  try {
-    const patientData = req.body;
+    try {
+        const patientData = req.body;
 
-    if (req.file) {
-      // Upload the PDF file to Cloudinary
-      cloudinary.uploader.upload_stream(
-        { resource_type: 'raw', public_id: `patient_files/${patientData?.firstName + " " + patientData?.lastName}${Date.now()}.pdf` },
-        async (error, result) => {
-          if (error) {
-            console.error(error);
-            return next(error);
-          }
+        if (req.file) {
+            // Upload the PDF file to Cloudinary
+            cloudinary.uploader
+                .upload_stream(
+                    {
+                        resource_type: "raw",
+                        public_id: `patient_files/${
+                            patientData?.firstName + " " + patientData?.lastName
+                        }${Date.now()}.pdf`,
+                    },
+                    async (error, result) => {
+                        if (error) {
+                            console.error(error);
+                            return next(error);
+                        }
 
-          patientData.pdfURL = result.secure_url;
+                        patientData.pdfURL = result.secure_url;
 
-          try {
-            const newPatient = await PatientRepo.createPatient(patientData);
-            successResponse(res, 'Patient created successfully.', newPatient, 201);
-          } catch (dbError) {
-            next(dbError); 
-          }
+                        try {
+                            const newPatient = await PatientRepo.createPatient(
+                                patientData
+                            );
+                            successResponse(
+                                res,
+                                "Patient created successfully.",
+                                newPatient,
+                                201
+                            );
+                        } catch (dbError) {
+                            next(dbError);
+                        }
+                    }
+                )
+                .end(req.file.buffer);
+        } else {
+            try {
+                const newPatient = await PatientRepo.createPatient(patientData);
+                successResponse(
+                    res,
+                    "Patient created successfully.",
+                    newPatient,
+                    201
+                );
+            } catch (dbError) {
+                next(dbError);
+            }
         }
-      ).end(req.file.buffer);
-    } else {
-      try {
-        const newPatient = await PatientRepo.createPatient(patientData);
-        successResponse(res, 'Patient created successfully.', newPatient, 201);
-      } catch (dbError) {
-        next(dbError); 
-      }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 /**
@@ -66,16 +92,16 @@ const createPatient = async (req, res, next) => {
  * @returns {PatientModel}
  */
 const getPatientById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const patient = await PatientRepo.findOnePatientByObject({ _id: id });
-    if (!patient) {
-      return badRequest(res, 'Patient not found.', []);
+    try {
+        const { id } = req.params;
+        const patient = await PatientRepo.findOnePatientByObject({ _id: id });
+        if (!patient) {
+            return badRequest(res, "Patient not found.", []);
+        }
+        successResponse(res, "Patient retrieved successfully.", patient, 200);
+    } catch (error) {
+        next(error);
     }
-    successResponse(res, 'Patient retrieved successfully.', patient, 200);
-  } catch (error) {
-    next(error);
-  }
 };
 
 /**
@@ -84,40 +110,61 @@ const getPatientById = async (req, res, next) => {
  * @returns {PatientModel}
  */
 const updatePatient = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const data = req.body;
-    console.log('data', data,req.body);
-    if (req.file) {
-      cloudinary.uploader.upload_stream(
-        { resource_type: 'raw', public_id: `patient_files/${data?.firstName + " " + data?.lastName}${Date.now()}.pdf` },
-        async (error, result) => {
-          if (error) {
-            console.error(error);
-            return next(error);
-          }
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        console.log("data", data, req.body);
+        if (req.file) {
+            cloudinary.uploader
+                .upload_stream(
+                    {
+                        resource_type: "raw",
+                        public_id: `patient_files/${
+                            data?.firstName + " " + data?.lastName
+                        }${Date.now()}.pdf`,
+                    },
+                    async (error, result) => {
+                        if (error) {
+                            console.error(error);
+                            return next(error);
+                        }
 
-          data.pdfURL = result.secure_url;
+                        data.pdfURL = result.secure_url;
 
-          try {
-            const updatedPatient = await PatientRepo.updatePatient(id, data);
-            successResponse(res, 'Patient updated successfully.', updatedPatient, 200);
-          } catch (dbError) {
-            next(dbError); 
-          }
+                        try {
+                            const updatedPatient =
+                                await PatientRepo.updatePatient(id, data);
+                            successResponse(
+                                res,
+                                "Patient updated successfully.",
+                                updatedPatient,
+                                200
+                            );
+                        } catch (dbError) {
+                            next(dbError);
+                        }
+                    }
+                )
+                .end(req.file.buffer);
+        } else {
+            try {
+                const updatedPatient = await PatientRepo.updatePatient(
+                    id,
+                    data
+                );
+                successResponse(
+                    res,
+                    "Patient updated successfully.",
+                    updatedPatient,
+                    200
+                );
+            } catch (dbError) {
+                next(dbError);
+            }
         }
-      ).end(req.file.buffer);
-    } else {
-      try {
-        const updatedPatient = await PatientRepo.updatePatient(id, data);
-        successResponse(res, 'Patient updated successfully.', updatedPatient, 200);
-      } catch (dbError) {
-        next(dbError); 
-      }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 /**
@@ -125,63 +172,90 @@ const updatePatient = async (req, res, next) => {
  * @returns {boolean}
  */
 const deletePatient = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const isDeleted = await PatientRepo.deletePatient(id);
-    if (!isDeleted) {
-      return badRequest(res, 'Patient not found.', []);
+    try {
+        const { id } = req.params;
+        const isDeleted = await PatientRepo.deletePatient(id);
+        if (!isDeleted) {
+            return badRequest(res, "Patient not found.", []);
+        }
+        successResponse(res, "Patient deleted successfully.", [], 200);
+    } catch (error) {
+        next(error);
     }
-    successResponse(res, 'Patient deleted successfully.', [], 200);
-  } catch (error) {
-    next(error);
-  }
 };
 
+const patientUpdateRequest = async (req, res, next) => {
+    const { id, patientName } = req.body;
+    console.log("patient", patientName);
 
-const patientUpdateRequest = async (req, res, next) =>{
-  const {id, patientName} = req.body
-  console.log('patient', patientName);
-  try {
-    const mailOptions = {
-      from: 'sohailshabir282@gmail.com',
-      to: "codezone67@gmail.com",
-      subject: 'Request for Update',
-      html: `
+    try {
+        const patient = await patientModel.findOne({ _id: id });
+        const user = await userModel.findOne({
+            _id: patient.consultant.toString(),
+        });
+        const mailOptions = {
+            from: "sohailshabir282@gmail.com",
+            to: "naveed@mailinator.com",
+            subject: "Request for Update",
+            html: `
         <p>Dear Admin,</p>
         <p>We hope this message finds you well.</p>
         <p>We would like to request an update regarding referral of patient ${patientName}. Please provide any additional information or updates that may be relevant to your case.</p>
         <p>Thank you for your cooperation.</p>
         <p>Best regards,</p>
-        <p>Your Organization</p>
+        <p>From:${user.name}</p>
       `,
-    };
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: Number(process.env.EMAIL_PORT),
-      secure: Boolean(process.env.SECURE),
-      auth : {
-          user: process.env.USER,
-          pass : process.env.PASSWORD
-      }
-  })
+        };
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASSWORD,
+            },
+        });
 
-    const info = await transporter.sendMail(mailOptions);
-    const updatedPatient = await PatientRepo.updatePatient(id, {updateRequest : true})
-    return successResponse(res, 'Update Reminder Sent', updatedPatient , 200)
-  }catch(error) {
-    next(error)
-  }
-}
+        const info = await transporter.sendMail(mailOptions);
+        const updatedPatient = await PatientRepo.updatePatient(id, {
+            updateRequest: true,
+        });
 
-const patientProfile = async (req, res, next) =>{
-  const {pdfURL, id, name} = req.body
-  try {
-    const mailOptions = {
-      from: 'sohailshabir282@gmail.com',
-      to: "codezone67@gmail.com",
-      subject: 'Request for Update',
-      html: `
+        if (patient?.lastTimeMailSent > Date.now()) {
+            console.log("Cant sent mail wait 12 hours");
+            return errorResponse(
+                res,
+                "Email already sent",
+                [],
+                httpStatus.INTERNAL_SERVER_ERROR
+            );
+        } else {
+            console.log("last time updated");
+            await patientModel.findOneAndUpdate(
+                { _id: id },
+                { lastTimeMailSent: Date.now() + 43200000 }
+            );
+            return successResponse(
+                res,
+                "Update Reminder Sent",
+                updatedPatient,
+                200
+            );
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+const patientProfile = async (req, res, next) => {
+    const { pdfURL, id, name } = req.body;
+    try {
+        const mailOptions = {
+            from: "sohailshabir282@gmail.com",
+            to: "codezone67@gmail.com",
+            subject: "Request for Update",
+            html: `
         <p>Dear Admin,</p>
         <p>We hope this message finds you well.</p>
         <p>We would like to request an update regarding referral of patient ${name}. Please provide any additional information or updates that may be relevant to your case.</p>
@@ -190,74 +264,102 @@ const patientProfile = async (req, res, next) =>{
         <p>Best regards,</p>
         <p>Your Organization</p>
       `,
-    };
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: Number(process.env.EMAIL_PORT),
-      secure: Boolean(process.env.SECURE),
-      auth : {
-          user: process.env.USER,
-          pass : process.env.PASSWORD
-      }
-  })
+        };
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASSWORD,
+            },
+        });
 
-    const info = await transporter.sendMail(mailOptions);
-    if (info) {
-      const updatePatient = await PatientRepo?.updatePatient(id, {pending:true})
-      return successResponse(res, "Request Update Sent Successfully", info, 200)
-    }else{
-      return errorResponse(res, "Something went wrong please try again ", [])
-    }
-}catch(error) {
-  next(error)
-}
-}
-
-
-
-const uploadReportByAdmin = async (req, res, next) => {
-  const { id } = req.params;
-
-  try {
-    if (req.file) {
-
-      cloudinary.uploader.upload_stream(
-        { resource_type: 'raw', public_id: `patient_files/${Date.now()}.pdf` },
-        async (error, result) => {
-          if (error) {
-            console.error('error===>',error);
-            return errorResponse(res, "Error uploading report", error);
-          }
-
-          const finalReport = result.secure_url;
-          console.log('finalReport====', finalReport);
-          const updatedPatient = await PatientRepo.updatePatient(id, { finalReport, adminResponse: true });
-
-          if (!updatedPatient) {
-            return badRequest(res, "Something went wrong, please try again", []);
-          }
-
-          return successResponse(res, "Report submitted successfully", updatedPatient, 200);
+        const info = await transporter.sendMail(mailOptions);
+        if (info) {
+            const updatePatient = await PatientRepo?.updatePatient(id, {
+                pending: true,
+            });
+            return successResponse(
+                res,
+                "Request Update Sent Successfully",
+                info,
+                200
+            );
+        } else {
+            return errorResponse(
+                res,
+                "Something went wrong please try again ",
+                []
+            );
         }
-      ).end(req.file.buffer);;
-    } else {
-      return errorResponse(res, "No file provided", []);
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    console.error('Unhandled error:', error);
-    next(error)
-  }
 };
 
+const uploadReportByAdmin = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        if (req.file) {
+            cloudinary.uploader
+                .upload_stream(
+                    {
+                        resource_type: "raw",
+                        public_id: `patient_files/${Date.now()}.pdf`,
+                    },
+                    async (error, result) => {
+                        if (error) {
+                            console.error("error===>", error);
+                            return errorResponse(
+                                res,
+                                "Error uploading report",
+                                error
+                            );
+                        }
+
+                        const finalReport = result.secure_url;
+                        console.log("finalReport====", finalReport);
+                        const updatedPatient = await PatientRepo.updatePatient(
+                            id,
+                            { finalReport, adminResponse: true, pending: false }
+                        );
+
+                        if (!updatedPatient) {
+                            return badRequest(
+                                res,
+                                "Something went wrong, please try again",
+                                []
+                            );
+                        }
+
+                        return successResponse(
+                            res,
+                            "Report submitted successfully",
+                            updatedPatient,
+                            200
+                        );
+                    }
+                )
+                .end(req.file.buffer);
+        } else {
+            return errorResponse(res, "No file provided", []);
+        }
+    } catch (error) {
+        console.error("Unhandled error:", error);
+        next(error);
+    }
+};
 
 module.exports = {
-  getAllPatients,
-  createPatient,
-  getPatientById,
-  updatePatient,
-  deletePatient,
-  patientUpdateRequest,
-  patientProfile,
-  uploadReportByAdmin
+    getAllPatients,
+    createPatient,
+    getPatientById,
+    updatePatient,
+    deletePatient,
+    patientUpdateRequest,
+    patientProfile,
+    uploadReportByAdmin,
 };
--- models/PatientModel.js
+++ models/PatientModel.js
@@ -1,131 +1,136 @@
 const mongoose = require("mongoose");
-const PatientModelName = 'Patient';
+const PatientModelName = "Patient";
 
 const Schema = mongoose.Schema;
 
-let Patient = new Schema({
-    title: {
-        type: String,
-        required: true,
+let Patient = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        dob: {
+            type: Date,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+            enum: ["Male", "Female", "Prefer Not to Say"],
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+
+        consultant: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            default: "",
+        },
+        postalCode: {
+            type: String,
+            default: "",
+        },
+        city: {
+            type: String,
+            default: "",
+        },
+        bodyPart: {
+            type: String,
+        },
+        clinicalIndication: {
+            type: String,
+        },
+        payment: {
+            type: String,
+            required: true,
+            enum: ["Self Provider", "Insurance/Embassy"],
+        },
+        paymentMethod: {
+            type: String,
+        },
+        provider: {
+            type: String,
+        },
+        policyNumber: {
+            type: String,
+        },
+        clinicalInfo: {
+            type: [String],
+        },
+        safety: {
+            pacemaker: {
+                type: String,
+                enum: ["Yes", "No"],
+            },
+            eyeInjury: {
+                type: String,
+                enum: ["Yes", "No"],
+            },
+            pregnancy: {
+                type: String,
+                enum: ["Yes", "No", "Not applicable", "Unknown"],
+            },
+            eGFR: {
+                type: String,
+            },
+        },
+        pdfURL: {
+            type: String,
+            default: "",
+        },
+        active: {
+            type: Boolean,
+            default: true,
+        },
+        status: {
+            type: Boolean,
+            default: false,
+        },
+        approved: {
+            type: Boolean,
+            default: false,
+        },
+        pending: {
+            type: Boolean,
+            default: false,
+        },
+        updateRequest: {
+            type: Boolean,
+            default: false,
+        },
+        adminResponse: {
+            type: Boolean,
+            default: false,
+        },
+        finalReport: {
+            type: String,
+            default: "",
+        },
+        lastTimeMailSent: {
+            type: Number,
+        },
     },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    dob: {
-        type: Date,
-        required: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-        enum: ['Male', 'Female', 'Prefer Not to Say'],
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    
-  consultant: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        default:""
-    },
-    postalCode: {
-        type: String,
-        default:""
-    },
-    city: {
-        type: String,
-        default:""
-    },
-    bodyPart: {
-        type: String,
-    },
-    clinicalIndication: {
-        type: String,
-    },
-    payment: {
-        type: String,
-        required: true,
-        enum: ['Self Provider', 'Insurance/Embassy'],
-    },
-    paymentMethod: {
-        type: String,
-    },
-    provider: {
-        type: String,
-    },
-    policyNumber: {
-        type: String,
-    },
-    clinicalInfo: {
-      type: [String],
-    },
-    safety: { 
-      pacemaker: {
-        type: String,
-        enum: ['Yes', 'No'],
-      },
-      eyeInjury: {
-        type: String,
-        enum: ['Yes', 'No'],
-      },
-      pregnancy: {
-        type: String,
-        enum: ['Yes', 'No', 'Not applicable', 'Unknown'],
-      },
-      eGFR: {
-        type: String,
-      }
-    },
-    pdfURL: {
-        type: String,
-        default: ''
-    },
-    active: {
-        type: Boolean,
-        default: true,
-    },
-    status : {
-        type : Boolean,
-        default: false
-    },
-    approved: {
-        type: Boolean,
-        default : false
-    },
-    pending:{
-        type:Boolean,
-        default:false
-    },
-    updateRequest: {
-        type: Boolean,
-        default : false
-    },
-    adminResponse : {
-        type : Boolean,
-        default: false
-    },
-    finalReport : {
-        type : String,
-        default: ""
+    {
+        timestamps: true,
+        collection: PatientModelName,
     }
-},
-{ 
-    timestamps: true,
-    collection: PatientModelName,
-});
+);
 
 module.exports = mongoose.model(PatientModelName, Patient);
