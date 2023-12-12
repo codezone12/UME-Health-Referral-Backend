83257508f335ab92dd21b1dc632e20b5ecdc5492
Naveed Ali <naveed@digimarkdevelopers.com>
2023-12-13 00:09:54 +0500

    added fistname, lastname, title to user model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,78 +1,90 @@
 const mongoose = require("mongoose");
-const UserModelName = 'User'
+const UserModelName = "User";
 
 const Schema = mongoose.Schema;
 
-let User = new Schema({
-  name: {
-    type: String,
-  },
-  image: {
-    type: String,
-    default: 'https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg'
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },  
-  practitionerSpecialty : {
-    type: String,
-    default: ""
-  },
-  practitionerGroup : {
-    type: String,
-    default: ""
-  },
-  primaryPracticeName : {
-    type: String,
-    default: ""
-  },
-  registrationNumber : {
-    type: String,
-    default: ""
-  },
-  street : {
-    type: String,
-    default: ""
-  },
-  jobTitle : {
-    type: String,
-    default: ""
-  },
-  postcode : {
-    type: String,
-    default: ""
-  },
-  landline : {
-    type: String,
-    default: ""
-  },
-  townCity : {
-    type: String,
-    default: ""
-  },
-  role: {
-    type: String,
-    default:"consultant"
-  },
-  active: {
-    type: Boolean,
-    default: true
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  source : {
-    type : String,
-    default : ""
-  }
-},
-{ 
-  timestamps: true,
-  collection: UserModelName 
-});
+let User = new Schema(
+    {
+        title: {
+            type: String,
+        },
+        firstname: {
+            type: String,
+        },
+        lastname: {
+            type: String,
+        },
+        name: {
+            type: String,
+        },
+        image: {
+            type: String,
+            default:
+                "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
+        },
+        email: {
+            type: String,
+        },
+        password: {
+            type: String,
+        },
+        practitionerSpecialty: {
+            type: String,
+            default: "",
+        },
+        practitionerGroup: {
+            type: String,
+            default: "",
+        },
+        primaryPracticeName: {
+            type: String,
+            default: "",
+        },
+        registrationNumber: {
+            type: String,
+            default: "",
+        },
+        street: {
+            type: String,
+            default: "",
+        },
+        jobTitle: {
+            type: String,
+            default: "",
+        },
+        postcode: {
+            type: String,
+            default: "",
+        },
+        landline: {
+            type: String,
+            default: "",
+        },
+        townCity: {
+            type: String,
+            default: "",
+        },
+        role: {
+            type: String,
+            default: "consultant",
+        },
+        active: {
+            type: Boolean,
+            default: true,
+        },
+        verified: {
+            type: Boolean,
+            default: false,
+        },
+        source: {
+            type: String,
+            default: "",
+        },
+    },
+    {
+        timestamps: true,
+        collection: UserModelName,
+    }
+);
 
 module.exports = mongoose.model(UserModelName, User);
