365f047b15964fc3831378dacc314a19361876c1
codezone10 <codezone108@gmail.com>
2023-12-18 06:48:11 -0800

    Content

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 const otpMail = async (email, name, subject, otp) => {
-  try {
-    const emailHtml = `
+    try {
+        const emailHtml = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -18,29 +18,29 @@ const otpMail = async (email, name, subject, otp) => {
         </body>
         </html>
     `;
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: Number(process.env.EMAIL_PORT),
-      secure: Boolean(process.env.SECURE),
-      auth: {
-        user: process.env.USER,
-        pass: process.env.PASSWORD,
-      },
-    });
-    await transporter.sendMail({
-      to: email,
-      subject: subject,
-      html: emailHtml,
-    });
-    console.log("Email sent Successfully");
-  } catch (error) {
-    console.log(`Email not sent ${error}`);
-  }
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASSWORD,
+            },
+        });
+        await transporter.sendMail({
+            to: email,
+            subject: subject,
+            html: emailHtml,
+        });
+        console.log("Email sent Successfully");
+    } catch (error) {
+        console.log(`Email not sent ${error}`);
+    }
 };
 
 const otpRequest = async (firstName, lastName, otp, email, subject) => {
-  const emailHtml = `
+    const emailHtml = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -62,64 +62,6 @@ const otpRequest = async (firstName, lastName, otp, email, subject) => {
     </html>    
     `;
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    service: process.env.SERVICE,
-    port: Number(process.env.EMAIL_PORT),
-    secure: Boolean(process.env.SECURE),
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASSWORD,
-    },
-  });
-  const resp = await transporter.sendMail({
-    to: email,
-    subject: subject,
-    html: emailHtml,
-  });
-  if (resp) {
-    console.log("Email sent Successfully");
-  } else {
-    console.log("Email sent Failure");
-  }
-};
-
-const referralConfirmation = async (name, email, subject) => {
-<<<<<<< HEAD
-    // const emailHtml = `<!DOCTYPE html>
-    // <html lang="en">
-    // <head>
-    //   <meta charset="UTF-8">
-    //   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //   <title>A new UME Health referral has been created</title>
-    // </head>
-    // <body>
-    //   <p>Hello,</p>
-=======
-  const emailHtml = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>A new UME Health referral has been created</title>
-    </head>
-    <body>
-      <p>Hello,</p>
->>>>>>> 972f1de75d248979d14e2ae4f554dc18a6c4c8d2
-    
-    //   <p>A new referral request has been submitted by <strong>${name}</strong>. UME Health will aim to respond to the referral request within 48 hours.</p>
-    
-    //   <p>If you need any further assistance, please send us an email at <a href="mailto:clientrelations@umegroup.com">clientrelations@umegroup.com</a></p>
-    
-<<<<<<< HEAD
-    //   <p>Regards,<br>
-    //   UME Health Client Relations Team</p>
-    // </body>
-    // </html>
-    // `
-    ;
     const transporter = nodemailer.createTransport({
         host: process.env.HOST,
         service: process.env.SERVICE,
@@ -140,54 +82,65 @@ const referralConfirmation = async (name, email, subject) => {
     } else {
         console.log("Email sent Failure");
     }
-=======
-      <p>Regards,<br>
-      UME Health Client Relations Team</p>
-    </body>
-    </html>
-    `;
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    service: process.env.SERVICE,
-    port: Number(process.env.EMAIL_PORT),
-    secure: Boolean(process.env.SECURE),
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASSWORD,
-    },
-  });
-  const resp = await transporter.sendMail({
-    to: email,
-    subject: subject,
-    html: emailHtml,
-  });
-  if (resp) {
-    console.log("Email sent Successfully");
-  } else {
-    console.log("Email sent Failure");
-  }
->>>>>>> 972f1de75d248979d14e2ae4f554dc18a6c4c8d2
 };
 
-const referralConfirm = async (name, email, subject, pdfLink) => {
-  const emailHtml = `<!DOCTYPE html>
+const referralConfirmation = async (name, email, subject) => {
+    const emailHtml = `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Your UME Health Patient Referral</title>
+      <title>A new UME Health referral has been created</title>
+    </head>
+    <body>
+      <p>Hello,</p>
+    
+      <p>A new referral request has been submitted by <strong>${name}</strong>. UME Health will aim to respond to the referral request within 48 hours.</p>
+    
+      <p>If you need any further assistance, please send us an email at <a href="mailto:clientrelations@umegroup.com">clientrelations@umegroup.com</a></p>
+    
+      <p>Regards,<br>
+      UME Health Client Relations Team</p>
+    </body>
+    </html>
+    `;
+    const transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        service: process.env.SERVICE,
+        port: Number(process.env.EMAIL_PORT),
+        secure: Boolean(process.env.SECURE),
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD,
+        },
+    });
+    const resp = await transporter.sendMail({
+        to: email,
+        subject: subject,
+        html: emailHtml,
+    });
+    if (resp) {
+        console.log("Email sent Successfully");
+    } else {
+        console.log("Email sent Failure");
+    }
+};
+
+const referralConfirm = async (name, email, subject, pdfLink) => {
+    const emailHtml = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Referral created</title>
     </head>
     <body>
       <p>Hello!, ${name}</p>
     
     
-<<<<<<< HEAD
-      <p>UME Health has submitted a patient referral and you can find it under the UME Health patients referral portal by clicking on <a href="www.refer.umehealth.co.uk">www.refer.umehealth.co.uk</a> . 
-      If you need any further assistance, please send us an email at <a href="mailto:clientrelations@umegroup.com">clientrelations@umegroup.com</a></p>
-=======
       <p>A new referral request has been submitted by ${name}. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
->>>>>>> 865d649c55fedb8f1361bb4c9a497a99c02b6819
     
       <p>Regards,<br>
       UME Health Client Relations Team</p>
@@ -212,76 +165,30 @@ const referralConfirm = async (name, email, subject, pdfLink) => {
     </body>
     </html>
     `;
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    service: process.env.SERVICE,
-    port: Number(process.env.EMAIL_PORT),
-    secure: Boolean(process.env.SECURE),
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASSWORD,
-    },
-  });
-  const resp = await transporter.sendMail({
-    to: email,
-    subject: subject,
-    html: emailHtml,
-  });
-  if (resp) {
-    console.log("Email sent Successfully");
-  } else {
-    console.log("Email sent Failure");
-  }
-};
-
-const resetPasswordEmail = async (resetUrl, email, subject) => {
-  const emailHtml = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Reset Your Password</title>
-      </head>
-      <body>
-        <p>Dear,</p>
-      
-        <p>Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: <a href=${resetUrl}>${resetUrl}</a> </p>
-      
-        <p>If you didn't forget your password, please ignore this email!</p>
-      
-        <p>Regards,<br>
-        UME Health Client Relations Team</p>
-      </body>
-      </html>    
-      `;
-
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    service: process.env.SERVICE,
-    port: Number(process.env.EMAIL_PORT),
-    secure: Boolean(process.env.SECURE),
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASSWORD,
-    },
-  });
-  const resp = await transporter.sendMail({
-    to: email,
-    subject: subject,
-    html: emailHtml,
-  });
-  if (resp) {
-    console.log("Email sent Successfully");
-  } else {
-    console.log("Email sent Failure");
-  }
+    const transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        service: process.env.SERVICE,
+        port: Number(process.env.EMAIL_PORT),
+        secure: Boolean(process.env.SECURE),
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD,
+        },
+    });
+    const resp = await transporter.sendMail({
+        to: email,
+        subject: subject,
+        html: emailHtml,
+    });
+    if (resp) {
+        console.log("Email sent Successfully");
+    } else {
+        console.log("Email sent Failure");
+    }
 };
 module.exports = {
-  otpMail,
-  otpRequest,
-  referralConfirmation,
-  referralConfirm,
-  resetPasswordEmail,
+    otpMail,
+    otpRequest,
+    referralConfirmation,
+    referralConfirm,
 };
