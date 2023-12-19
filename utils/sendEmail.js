9b1d0c3af050a2f92ee19c18b46637524b0bcfc0
codezone10 <codezone108@gmail.com>
2023-12-19 01:06:31 -0800

    Resolve

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -97,7 +97,7 @@ const referralConfirmation = async (name, email, subject, pdfLink) => {
       <p>Hello!</p>
     
     
-      <p>A new referral request has been submitted by <strong> ${name} </strong>. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
+      <p>A new referral request has been submitted by consultant <strong> ${name} </strong>. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
     
       <p>Regards,<br>
       UME Health Client Relations Team</p>
@@ -159,7 +159,7 @@ const referralConfirm = async (name, email, subject, pdfLink) => {
       <p>Hello!</p>
     
     
-      <p>A new referral request has been submitted by <strong> ${name} </strong>. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
+      <p>A new referral request has been submitted by patient <strong> ${name} </strong>. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
     
       <p>Regards,<br>
       UME Health Client Relations Team</p>
