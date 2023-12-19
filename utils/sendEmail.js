21fee027b29ff878b0d5fd8e642e3827897e9ef7
codezone10 <codezone108@gmail.com>
2023-12-19 01:21:09 -0800

    Resolving

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -83,7 +83,7 @@ const otpRequest = async (firstName, lastName, otp, email, subject) => {
         console.log("Email sent Failure");
     }
 };
-admin
+
 const referralConfirmation = async (name, email, subject, pdfLink) => {
     const emailHtml = `<!DOCTYPE html>
     <html lang="en">
@@ -221,7 +221,7 @@ const informConsultant = async (name, email, subject, pdfLink) => {
       <p>Hello!</p>
     
     
-      <p>A new referral request has been submitted by <strong> ${name}  </strong>. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
+      <p>A new referral request has been submitted by <strong>  </strong>. You can see a copy of referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
     
       <p>Regards,<br>${name}
       UME Health Client Relations Team</p>
