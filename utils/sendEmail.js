9373a1ed03bb49a0bdb5813367db442726252013
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-01-05 22:10:18 +0500

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -152,13 +152,13 @@ const referralConfirmed = async (name, email, subject, pdfLink) => {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Referral created</title>
+      <title>Referral eated</title>
     </head>
     <body>
-      <p>Hello!</p>
-      
-
-      <p>A new referral request has been submitted by <strong>${name}</strong>. You can see a copy of the referral by <a href=${pdfLink}>clicking here</a>. UME Health will aim to respond to the referral request within 48 hours.</p>
+      <p>Hello ${name},</p>
+    
+    
+      <p>UME Health has received an imagining referral for you submitted by <strong> ${name} </strong. Our bookings team will be in touch with you to book your appointment. Rest assured your referral is in safe hands. If you do need to get in touch, please email <a href="mailto:bookings@umegroup.com">bookings@umegroup.com</a></p>
     
       <p>Regards,<br>
       UME Health Client Relations Team</p>
@@ -167,19 +167,22 @@ const referralConfirmed = async (name, email, subject, pdfLink) => {
       Telephone: 0207 467 6190<br>
       Email: <a href="mailto:bookings@umegroup.com">bookings@umegroup.com</a><br>
       Web: www.umehealth.co.uk<br>
-    </p>
+    </div>
+    
+<p>
 
-    <h6>Disclaimer and Confidentiality Note:</h6>
+      <h6>Disclaimer and Confidentiality Note:</h6>
 
-    <p>Everything in this email and any attachments relating to the official business of UME Group LLP is proprietary to the company.</p>
+    Everything in this email and any attachments relating to the official business of UME Group LLP is proprietary to the company.
 
-    <p>It is confidential, legally privileged by law. UME does not own and endorse any other content. Views and opinions are those of the sender unless clearly stated as being that of UME Group.</p>
+    It is confidential, legally privileged by law. UME does not own and endorse any other content. Views and opinions are those of the sender unless clearly stated as being that of UME Group.
 
-    <p>The person addressed in the email is the sole authorized recipient. Please notify the sender immediately if it has unintentionally reached you and do not read, disclose or use the content in any way. Please destroy the communication and all attachments immediately.</p>
+    The person addressed in the email is the sole authorized recipient. Please notify the sender immediately if it has unintentionally reached you and do not read, disclose or use the content in any way. Please destroy the communication and all attachments immediately.
 
-    <p>UME Group cannot assure that the integrity of this communication has been maintained or that it is free from errors, virus, interception or interference.</p>
+    UME Group cannot assure that the integrity of this communication has been maintained or that it is free from errors, virus, interception or interference.
 
-    <p>UME Group LLP, 17 Harley St, London W1G 9QH, Tel: 020 7391 8660 Fax: 020 7391 8666</p>
+    UME Group LLP, 17 Harley St, London W1G 9QH, Tel: 020 7391 8660 Fax: 020 7391 8666
+    Registered in the UK. Registration number: OC333533
     </body>
     </html>
     `;
@@ -193,23 +196,9 @@ const referralConfirmed = async (name, email, subject, pdfLink) => {
             pass: process.env.PASSWORD,
         },
     });
-
     const resp = await transporter.sendMail({
         to: email,
-        subject:"Referral confirmation - your referral has been received" ,
-        html: emailHtml,
-    });
-
-    if (resp) {
-        console.log("Email sent Successfully");
-    } else {
-        console.log("Email sent Failure");
-    }
-};
-
-    const resp = await transporter.sendMail({
-        to: email,
-        subject: "Referral confirmation - your referral has been received",
+        subject: "Your appointment with UME Health, 17 Harley Street",
         html: emailHtml,
     });
     if (resp) {
