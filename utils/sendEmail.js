f81cd0c00d9ac4a46b66d4546ff14c28c8e2785b
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-01-05 20:24:35 +0500

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -153,7 +153,7 @@ const referralConfirm = async (name, email, subject, pdfLink) => {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Referral created</title>
+      <title>Referral eated</title>
     </head>
     <body>
       <p>Hello!</p>
@@ -215,7 +215,7 @@ const informConsultant = async (name, email, subject, pdfLink) => {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Referral created</title>
+      <title>Referral createded</title>
     </head>
     <body>
       <p>Hello!</p>
