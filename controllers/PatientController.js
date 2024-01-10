842205fbe5bd07e11940a4965b12616a65941417
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-01-10 12:47:46 +0500

    Update PatientController.js

--- controllers/PatientController.js
+++ controllers/PatientController.js
@@ -221,7 +221,7 @@ const patientUpdateRequest = async (req, res, next) => {
             subject: "Re: referral update request",
             html: `
                 <p>Hello,</p>
-                <p>Consultant <strong>${user.name} </strong> has requested an update on the referral they made for the patient <strong>${patientName}.</strong></p>
+                <p><strong>${user.name} </strong> has requested an update on the referral they made for the patient <strong>${patientName}.</strong></p>
                 <p>Regards, <br>
                 UME Health Client Relations Team</p>
                 <h2>Disclaimer and Confidentiality Note:</h2>
@@ -345,44 +345,24 @@ const patientUpdateRequest = async (req, res, next) => {
     }
 }; */
 
-
-const patientUpdateRequest = async (req, res, next) => {
-    console.log("patientUpdateRequest");
-    const { pdfURL, id, name, patientName: originalPatientName } = req.body;
-    console.log("patient", originalPatientName, name);
-
+const patientProfile = async (req, res, next) => {
+    console.log("patientProfile");
+    const { pdfURL, id, name } = req.body;
     try {
-        const patient = await patientModel.findOne({ _id: id });
-        const patientName = patient.title + " " + patient.firstName + " " + patient.lastName;
-        console.log("patientName:", patientName);
-
-        const user = await userModel.findOne({
-            _id: patient.consultant.toString(),
-        });
         const mailOptions = {
             from: "sohailshabir282@gmail.com",
-            to: "codezone67@gmail.com",
-            subject: "Re: referral update request",
+            to: "mubeen@mailinator.com",
+            subject: "Request for Update",
             html: `
-                <p>Hello,</p>
-                <p><strong>${user.name} </strong> has requested an update on the referral they made for the patient <strong>${patientName}.</strong></p>
-                <p>Regards, <br>
-                UME Health Client Relations Team</p>
-                <h2>Disclaimer and Confidentiality Note:</h2>
-
-                Everything in this email and any attachments relating to the official business of UME Group LLP is proprietary to the company.
-            
-                It is confidential, legally privileged by law. UME does not own and endorse any other content. Views and opinions are those of the sender unless clearly stated as being that of UME Group.
-            
-                The person addressed in the email is the sole authorized recipient. Please notify the sender immediately if it has unintentionally reached you and do not read, disclose or use the content in any way. Please destroy the communication and all attachments immediately.
-            
-                UME Group cannot assure that the integrity of this communication has been maintained or that it is free from errors, virus, interception or interference.
-            
-                UME Group LLP, 17 Harley St, London W1G 9QH, Tel: 020 7391 8660 Fax: 020 7391 8666
-                Registered in the UK. Registration number: OC333533
-            `,
+        <p>Hello,</p>
+        <p>We hope this message finds you well.</p>
+        <p>We would like to request an update regarding referral of patient ${name}. Please provide any additional information or updates that may be relevant to your case.</p>
+        <p>PDF is attached <a href=${pdfURL} > Here</a></p>
+        <p>Thank you for your cooperation.</p>
+        <p>Best regards,</p>
+        <p>Your Organization</p>
+      `,
         };
-
         const transporter = nodemailer.createTransport({
             host: process.env.HOST,
             service: process.env.SERVICE,
@@ -395,29 +375,21 @@ const patientUpdateRequest = async (req, res, next) => {
         });
 
         const info = await transporter.sendMail(mailOptions);
-        const updatedPatient = await PatientRepo.updatePatient(id, {
-            updateRequest: true,
-        });
-
-        if (patient?.lastTimeMailSent > Date.now()) {
-            console.log("Can't send mail, wait 12 hours");
-            return errorResponse(
-                res,
-                "A copy of the referral has been sent to your email",
-                [],
-                httpStatus.INTERNAL_SERVER_ERROR
-            );
-        } else {
-            console.log("last time updated");
-            await patientModel.findOneAndUpdate(
-                { _id: id },
-                { lastTimeMailSent: Date.now() + 43200000 }
-            );
+        if (info) {
+            const updatePatient = await PatientRepo?.updatePatient(id, {
+                pending: true,
+            });
             return successResponse(
                 res,
-                "A reminder has been sent to UME Health, and you should get an update shortly.",
-                updatedPatient,
+                "Request Update Sent Successfully",
+                info,
                 200
+            );
+        } else {
+            return errorResponse(
+                res,
+                "Something went wrong please try again ",
+                []
             );
         }
     } catch (error) {
@@ -425,6 +397,75 @@ const patientUpdateRequest = async (req, res, next) => {
     }
 };
 
+/* const uploadReportByAdmin = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        if (req.file) {
+            cloudinary.uploader
+                .upload_stream(
+                    {
+                        resource_type: "raw",
+                        public_id: `patient_files/${Date.now()}.pdf`,
+                    },
+                    async (error, result) => {
+                        if (error) {
+                            console.error("error===>", error);
+                            return errorResponse(
+                                res,
+                                "Error uploading report",
+                                error
+                            );
+                        }
+
+                        const finalReport = result.secure_url;
+                        console.log("finalReport====", finalReport);
+                        const updatedPatient = await PatientRepo.updatePatient(
+                            id,
+                            {
+                                finalReport,
+                                adminResponse: true,
+                                pending: false,
+                            }
+                        );
+
+                        if (!updatedPatient) {
+                            return badRequest(
+                                res,
+                                "Something went wrong, please try again",
+                                []
+                            );
+                        }
+                        console.log(
+                            "updatedPatient====",
+                            updatedPatient.consultant
+                        );
+                        const consultant = await userModel.findOne({
+                            _id: updatedPatient.consultant,
+                        });
+                        await informConsultant(
+                            consultant.name,
+                            consultant.email,
+                            "Re: Your UME Health Patient Referral",
+                            finalReport
+                        );
+                        return successResponse(
+                            res,
+                            "Report submitted successfully",
+                            updatedPatient,
+                            200
+                        );
+                    }
+                )
+                .end(req.file.buffer);
+        } else {
+            return errorResponse(res, "No file provided", []);
+        }
+    } catch (error) {
+        console.error("Unhandled error:", error);
+        next(error);
+    }
+}; */
 const uploadReportByAdmin = async (req, res, next) => {
     const { id } = req.params;
 
@@ -496,6 +537,7 @@ const uploadReportByAdmin = async (req, res, next) => {
         next(error);
     }
 };
+
 
 module.exports = {
     getAllPatients,
