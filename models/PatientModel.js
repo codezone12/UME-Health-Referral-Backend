117a85d65c3787831813468f5cd01eac1434aa10
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-01-22 20:10:36 +0500

    Update PatientModel.js

--- models/PatientModel.js
+++ models/PatientModel.js
@@ -93,7 +93,7 @@ let Patient = new Schema(
         pdfURL: {
             type: String,
             default: "",
-            unique: true,
+
         },
         active: {
             type: Boolean,
