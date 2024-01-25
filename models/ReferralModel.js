8b19f59d75600a6af001e0fc56322b9347761698
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-01-26 01:59:38 +0500

    Update ReferralModel.js

--- models/ReferralModel.js
+++ models/ReferralModel.js
@@ -5,54 +5,18 @@ const Schema = mongoose.Schema;
 
 let Referral = new Schema(
     {
-
-    
-      
-        
-      
+        _id: {
+            type: Schema.Types.ObjectId,
+            auto: true,
+        },
 
         consultant: {
             type: Schema.Types.ObjectId,
             ref: "User",
         },
-     bodyPart: {
+        bodyPart: {
             type: String,
         },
-        clinicalIndication: {
-            type: String,
-        },
-
-      
-        clinicalInfo: {
-            type: [String],
-        },
-        eGFR: { type: String, },  // Adding eGFR field based on provided data
-        metalImplant: { type: String, },
-        eyeInjury: { type: String, },
-        date: { type: Date, },
-        // Ensure that the unique index is set in your MongoDB schema
-        pdfURL: {
-            type: String,
-            default: "null",
-            unique: true,
-        },
-
-
-      
-        lastTimeMailSent: {
-            type: Number,
-        },
-    },
-    {
-        timestamps: true,
-        collection: ReferralModelName,
-    }
-);
-
-/* 
-let Referral = new Schema(
-    {
-
         clinicalIndication: {
             type: String,
         },
@@ -68,34 +32,6 @@ let Referral = new Schema(
             default: "null",
             unique: true,
         },
-        active: {
-            type: Boolean,
-            default: true,
-        },
-        status: {
-            type: Boolean,
-            default: false,
-        },
-        approved: {
-            type: Boolean,
-            default: false,
-        },
-        pending: {
-            type: Boolean,
-            default: false,
-        },
-        updateRequest: {
-            type: Boolean,
-            default: false,
-        },
-        adminResponse: {
-            type: Boolean,
-            default: false,
-        },
-        finalReport: {
-            type: String,
-            default: "",
-        },
         lastTimeMailSent: {
             type: Number,
         },
@@ -104,6 +40,6 @@ let Referral = new Schema(
         timestamps: true,
         collection: ReferralModelName,
     }
-); */
+);
 
-module.exports = mongoose.model(ReferralModelName, Referral); 
+module.exports = mongoose.model(ReferralModelName, Referral);
