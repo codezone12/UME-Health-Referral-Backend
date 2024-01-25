6de181375ae1f23696b4004728136cfe5e323259
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-01-26 04:53:52 +0500

    Update ReferralRouter.js

--- routes/v1/ReferralRouter.js
+++ routes/v1/ReferralRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const PatientController = require('../../controllers/PatientController');
-const ReferralController = require('../../controllers/ReferralController')
+const ReferralController = require('../../controllers/Referralcontroller')
 const upload = require('../../middlewares/multer')
 
-
+router.get('/', ReferralController.getAllReferrals);
 router.post('/', upload.single('pdf'), ReferralController.createReferral);
 
 
