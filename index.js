c31e9818e8cc158d95b8b06266f6908e7788ac33
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-03-15 14:52:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,35 @@ const config = require("./config/config");
 const logger = require("./config/logger");
 
 let server;
+
 mongoose.connect(config.mongoose.url, {
-    rejectUnauthorized: false,
     ...config.mongoose.options,
-    useFindAndModify: false, 
+    useFindAndModify: false,
+    useNewUrlParser: true, // Add this option
+    useUnifiedTopology: true // Add this option
 }).then(() => {
     logger.info("Connected to MongoDB");
     server = app.listen(config.port, () => {
-        logger.info(`Listening to port ${config.port}`);
+        logger.info(`Listening on port ${config.port}`);
     });
+}).catch((error) => {
+    logger.error("MongoDB connection error:", error);
+    process.exit(1);
 });
-
 
 const exitHandler = () => {
     if (server) {
         server.close(() => {
             logger.info("Server closed");
-            process.exit(1);
+            process.exit(0);
         });
     } else {
-        process.exit(1);
+        process.exit(0);
     }
 };
 
 const unexpectedErrorHandler = (error) => {
-    logger.error(error);
+    logger.error("An unexpected error occurred:", error);
     exitHandler();
 };
 
@@ -37,7 +41,5 @@ process.on("unhandledRejection", unexpectedErrorHandler);
 
 process.on("SIGTERM", () => {
     logger.info("SIGTERM received");
-    if (server) {
-        server.close();
-    }
+    exitHandler();
 });
